954323a596deb5deec86515ee9a85740f756a54c
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-03-25 18:09:18 +0530

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('hope you all are doing well'));
+app.get('/', (req, res) => res.send('cloud devOps'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
